adec18285cde6a2195720ef30398fba32b507272
rutekmar00 <46930922+rutekmar00@users.noreply.github.com>
2022-01-01 18:36:27 +0100

    Added LoginForm component
    - installed formik, yup and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      Page content
+      <LoginForm />
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const signInSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(7, "Password has at least 7 characters!")
+    .required("Required!"),
+  email: Yup.string().email("Invalid email!").required("Required!"),
+});
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      submitted: false,
+    };
+    this.resetSubmittedState = this.resetSubmittedState.bind(this);
+  }
+
+  resetSubmittedState() {
+    this.setState({
+      submitted: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="col-md-6 col-md-offset-3 mx-auto color-bckg">
+        <h1 className="p-4">Welcome back traveler!</h1>
+        <fieldset className="border p-4">
+          <legend className="w-auto">Ready? </legend>
+          <Formik
+            initialValues={{
+              password: "",
+              email: "",
+            }}
+            validationSchema={signInSchema}
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
+              if (!this.state.submitted) {
+                console.log("Log In successful");
+                console.log(values);
+                resetForm();
+                this.setState({ submitted: true });
+              }
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isValid,
+              dirty,
+            }) => (
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleSubmit(e);
+                }}
+              >
+                <div className={"form-group"}>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    aria-label="Email"
+                    autoComplete={"off"}
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <div className="alert alert-danger" role="alert">
+                      {errors.email}
+                    </div>
+                  )}
+                </div>
+                <div className={"form-group"}>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    aria-label="Password"
+                    autoComplete={"off"}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.password && touched.password && (
+                    <div className="alert alert-danger" role="alert">
+                      {errors.password}
+                    </div>
+                  )}
+                </div>
+                <div className="form-group">
+                  <button
+                    disabled={!(isValid && dirty)}
+                    className="btn btn-primary"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                  <button
+                    onClick={() => console.log("home")}
+                    className="btn btn-secondary ml-1"
+                  >
+                    Cancel
+                  </button>
+                </div>
+                <div className="form-group">
+                  <button
+                    onClick={() => console.log("signup")}
+                    className="btn btn-link"
+                  >
+                    Register
+                  </button>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </fieldset>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
