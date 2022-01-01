ea3f7c5057fd91c8165fb3f91b8cc2f3adb46db8
rutekmar00 <46930922+rutekmar00@users.noreply.github.com>
2022-01-01 19:08:06 +0100

    Added HomePage component
    - installed reactstrap
    - added ImageSlider component and images to show
    -

---
+++ src/components/HomePage/ImageSlider.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./ImageSlider.css";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+
+const items = [
+  {
+    src: require("./images/home.png"),
+    altText: "Feel like home!",
+    caption: "Feel like home!",
+  },
+  {
+    src: require("./images/mission.png"),
+    altText: "Were you consequent today?",
+    caption: "Were you consequent today?",
+  },
+  {
+    src: require("./images/types.png"),
+    altText: "Choose your destiny",
+    caption: "Choose your destiny",
+  },
+];
+
+const ImageSlider = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        className="carousel-item-tag"
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img
+          className="img-fluid mh-100 w-100"
+          src={item.src}
+          alt={item.altText}
+        />
+        <CarouselCaption captionHeader={item.caption} captionText="" />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <div>
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
+    </div>
+  );
+};
+
+export default ImageSlider;
